989a9aab5dfaef46fb030afaa572ddae65e8f88f
Raymond Yang <yangraymond2011@gmail.com>
2024-01-21 17:25:49 -0800

    Imported and Initialized Fox 3d model (Committing before testing Fox animations)

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,9 @@
-import { useRef, useState } from 'react'
+import { Suspense, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
 import { Canvas } from '@react-three/fiber';
 
 import Loader from '../components/Loader'
+import Fox from '../models/Fox';
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -110,6 +111,29 @@ const Contact = () => {
           </button>
         </form>
       </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />}>
+            <Fox 
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+
+        </Canvas>
+      </div>
+
     </section>
   )
 }
